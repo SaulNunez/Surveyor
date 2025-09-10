e9d9ccd3c1ab926687b0616850f2abbe0bad7493
Saul Nunez <saul.nunez99@gmail.com>
2025-09-10 16:25:22 -0600

    Add function in services to edit survey

--- src/services/surveyService.ts
+++ src/services/surveyService.ts
@@ -37,6 +37,28 @@ export async function createSurvey(title: string, description: string, userId: s
     }
 }
 
+export async function editSurvey(surveyId: string, userId: string, title: string, description: string) {
+    const survey = await SurveyModel.findById(surveyId).exec();
+
+    if(!survey) {
+        throw new NotFoundError('Survey not found');
+    }
+
+    if(survey.user.toString() !== userId) {
+        throw new NotFoundError('Survey not found');
+    }
+
+    survey.title = title;
+    survey.description = description;
+
+    await survey.save();
+    return {
+        id: survey._id,
+        title: survey.title,
+        description: survey.description
+    }
+}
+
 export async function deleteSurvey(surveyId: string, userId: string) {
     const survey = await SurveyModel.findById(surveyId).exec();
 

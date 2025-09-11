1acc125ca3e95ef0807de516c07034cfd859d553
Saul Nunez <saul.nunez99@gmail.com>
2025-09-11 15:22:59 -0600

    Make final changes to swagger options

--- src/swagger.ts
+++ src/swagger.ts
@@ -7,18 +7,19 @@ export default {
       description:
         "Backend API for Surveyor, a self-hosted survey application that allows users to create and take surveys.",
       license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
+        name: "Apache-2.0",
+        url: "https://spdx.org/licenses/Apache-2.0.html",
       },
       contact: {
         name: "Saul Nunez",
-        url: "https://saulnunez.com",
+        url: "https://github.com/SaulNunez/Surveyor",
         email: "saul.nunez99@gmail.com",
       },
     },
     servers: [
       {
         url: "http://localhost:3000",
+        description: "Local server",
       },
     ],
   },

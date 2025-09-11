d1b9f819e1d70741b19a1983352c40ce9e7d61da
Saul Nunez <saul.nunez99@gmail.com>
2025-09-11 14:52:46 -0600

    Swagger documentation for post and delete for survey routes

--- src/routes/surveyRoutes.ts
+++ src/routes/surveyRoutes.ts
@@ -3,8 +3,49 @@ import { createSurveyForUser, deleteExistingSurvey, editExistingSurvey, getSurve
 
 const router: Router = Router();
 
+/**
+ * @swagger
+ * /api/survey
+ * post:
+ *  summary: Creates a new survey for the authenticated user
+ *   requestBody:
+ *     required: true
+ *     content:
+ *       application/json:
+ *         schema:
+ *          type: object
+ *         required:
+ *          - title
+ *          - description
+ *         properties:
+ *           title:
+ *             type: string
+ *             description: Title of the survey
+ *           description:
+ *             type: string
+ *             description: A text displayed before the questions. This has markdown support for links and formatting.
+ *   responses:
+ *     201:
+ *       description: Created
+ */
 router.post('/survey', createSurveyForUser);
+
+/**
+ * @swagger
+ * /api/survey/{surveyId}:
+ *   delete:
+ *     summary: Edits an existing survey for the authenticated user
+ *     parameters:
+ *       - name: surveyId
+ *         in: path
+ *         description: Id of the survey we are deleting
+ *         required: true
+ *     responses:
+ *       201:
+ *         description: Deleted
+ */
 router.delete('/survey/:surveyId', deleteExistingSurvey);
+
 router.get('/survey/:surveyId', getSurveyDetails);
 router.put('/survey/:surveyId', editExistingSurvey)
 
